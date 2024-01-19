d04fee307457ff9c28f87758d9a912bc6e3b9a8e
minari314159 <127341132+minari314159@users.noreply.github.com>
2024-01-19 17:02:55 -0500

    Widgets connected

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,18 @@
-from streamlit_jupyter import StreamlitPatcher
-import seaborn as sns
-import numpy as np
+
 import streamlit as st
-import matplotlib.pyplot as plt
 import pandas as pd
 import plost
 
-st.set_page_config(layout='wide')
-StreamlitPatcher().jupyter()
-
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', 500)
+st.set_page_config(
+    page_title="Canada's Impact of Green House Gases Sector Analysis",
+    page_icon="🇨🇦",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 pd.set_option('display.float_format', lambda x: '%.1f' % x)
 
 with open('styles.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-st.sidebar.header('DashBoard ')
-st.sidebar.link_button("<- back to portfolio",
-               url="https://3-d-portfolio-pi.vercel.app", type='secondary')
-
-st.sidebar.subheader('Year')
-year_slider = st.sidebar.slider('Year', 1990, 2020, 2020)
-
-
-
-st.sidebar.markdown('''
----
-Created SJ Olsen.
-''')
-
-st.markdown('### Impact Analysis of Canadian Green House Gases by Sector')
-
-#Row A
-st.markdown('#### Global GHG per Sector')
 
 df_GHG = pd.read_csv('Data/GHG_Emissions.csv')
 df_GHG.rename(columns={
@@ -49,295 +28,133 @@ df_GHG.rename(columns={
     'Greenhouse gas emissions from other fuel combustion': 'Fuel Combustion',
     'Greenhouse gas emissions from bunker fuels': 'Bunker Fuel'}, inplace=True)
 
-df_compare = df_GHG[df_GHG['Entity'].isin(
-    ['World'])]
-df_compare = df_compare.drop(['Code'], axis=1).query(
-    'Year >= 1900 & Year <= 2020')
-cols = df_compare.iloc[:, 3:]
-plost.line_chart(
-    data=df_compare,
-    x='Year',
-    y=('Agriculture', 'Forestry', 'Waste', 'Buildings',
-            'Industry', 'Manufacturing and Construction', 'Electricity and Heat', 'Energy Production', 'Fuel Combustion', 'Bunker Fuel'),
-    width=600,
-    legend='top',
-    pan_zoom='minimap',
-    use_container_width=True
-)
-    
+st.sidebar.header('Impact of Green House Gases `Sector Analysis` ')
 
+st.sidebar.caption('Global greenhouse gas emissions continue to rise at a time when they need to be rapidly falling. To effectively reduce emissions, we need to know where they come from  which sectors contribute the most? How can we use this understanding to develop effective solutions and mitigation strategies?')
+st.sidebar.caption("This analysis and visualizations will focus on Canada. Canadians generally have the atitude that Canada is a very environmentally friendly country due to its emmense amount of land and nature as well as 'green' government policies. Here these ideas will  be questioned, comparing Canada's emissions to the world's by industrial sector.")
+
+st.sidebar.divider()
+
+st.sidebar.subheader('Continent Selection')
+df_compare = df_GHG[df_GHG['Entity'].isin(
+    ['World', 'Asia', 'Africa', 'Oceania', 'Europe', 'North America', 'South America'])].drop(['Code'], axis=1)
+df_compare = df_compare.query(
+    'Year >= 1900 & Year <= 2020')
+
+continent_options = df_compare.iloc[:,0].unique().tolist()
+selection = st.sidebar.selectbox(
+    label='Choose a Continent', 
+    options=continent_options)
+
+st.sidebar.subheader('Year')
+year_options = df_compare['Year'].unique().tolist()
+year = st.sidebar.select_slider(
+    label='Year Range', 
+    options=year_options,
+    value=(year_options[0],year_options[30]))
+st.write(year)
+
+st.sidebar.subheader('Choose a Green House Gas')
+choose_GHG = st.sidebar.selectbox('Select data', ('All', 'CO2','CH4','NOx'))
+
+st.sidebar.markdown('''
+---
+''')
+st.sidebar.link_button("<- back to portfolio", url="https://3-d-portfolio-pi.vercel.app", type='secondary')
+st.sidebar.markdown('| Created SJ Olsen ')
+st.sidebar.markdown('`Data published online at OurWorldInData.org`')
+
+#Row A
+st.subheader('Continental Green House Gases Emissions per Sector')
+
+
+df_compare['Year'] = pd.to_datetime(df_compare['Year'], format='%Y')
+
+col1,col2 = st.columns(2)
+with col1:
+    def create_linechart(data):
+        plost.line_chart(
+            data=data,
+            x='Year',
+            y=('Agriculture', 'Forestry', 'Waste', 'Buildings',
+                    'Industry', 'Manufacturing and Construction', 'Electricity and Heat', 'Energy Production', 'Fuel Combustion', 'Bunker Fuel'),
+            legend=None,
+            height=400,
+            use_container_width=True
+        )
+    
+    if selection == 'Africa':
+        create_linechart(df_compare[df_compare['Entity'].isin(['Africa'])])
+    elif selection == 'Asia':
+        create_linechart(df_compare[df_compare['Entity'].isin(['Asia'])])
+    elif selection == 'Europe':
+        create_linechart(df_compare[df_compare['Entity'].isin(['Europe'])])
+    elif selection == 'North America':
+        create_linechart(df_compare[df_compare['Entity'].isin(['North America'])])
+    elif selection == 'South America':
+        create_linechart(df_compare[df_compare['Entity'].isin(['South America'])])
+    elif selection == 'Oceania':
+        create_linechart(df_compare[df_compare['Entity'].isin(['Oceania'])])
+    else:
+        create_linechart(df_compare[df_compare['Entity'].isin(['World'])])
+
+
+df_Sector = pd.read_csv('Data/Global_GHG_Emissions_by_Sector.csv')
+
+with col2:
+    st.metric("Global Temperature Record `2023`", "14.98 °C", "0.17 °C", delta_color='inverse')
+
+    plost.pie_chart(
+        data=df_Sector,
+        theta='Share of global greenhouse gas emissions (%)',
+        color='Sub-sector',
+        legend='right',
+        height=290,
+        use_container_width=True)
+
+st.divider()
+#Row B
 ### Canada's Mean Greenhouse Gas Emission per sector
-st.markdown("#### Canada's Mean Greenhouse Gas Emission per sector")
+st.subheader("Canada's Greenhouse Gas Emission per Sector")
+col1, col2 = st.columns((4,6))
 
 df_GHG_Can = df_GHG[df_GHG['Entity'].isin(['Canada'])].drop([
     'Code'], axis=1)
-
-
-# +
-GHG_y = df_GHG_Can.iloc[:, 2:]
-
-fig, ax = plt.subplots(figsize=(15, 8))
-
-ax.plot(df_GHG_Can['Year'], GHG_y)
-fig.autofmt_xdate(which='both')
-
-ax.set_title('Canadian Green House Gas Emission per Sector')
-ax.set_ylabel('Green House Gas Emission (Tonnes)')
-ax.set_xlabel('Year')
-ax.legend(['Agriculture', 'Forestry',
-           'Waste',
-           'Buildings',
-           'Industry',
-           'Manufacturing and Construction',
-           'Transport',
-           'Electricity and Heat',
-           'Energy Production',
-           'Fuel Combustion',
-           'Bunker Fuel'])
-plt.tight_layout()
-plt.show()
-# -
-
-# ### Create Selector Button
-
-# ### Current Greenhouse Gase Emission Per Sector
-
-df_Current = df_GHG_Can.query('Year == 2020')
-df_Current = df_Current.drop(
-    ['Entity', 'Year'], axis=1).transpose().reset_index()
-df_Current = df_Current.rename(
-    columns={'index': 'Sub-sector', 1022: 'GHG Emission (Tonnes)'})
-df_Current["GHG Emission (%)"] = (
-    (df_Current['GHG Emission (Tonnes)']/sum(df_Current['GHG Emission (Tonnes)']))*100)
-df_Current = df_Current.interactive()
-
-# +
-
-GHG_y = df_Current['GHG Emission (%)']
-sub_sector = df_Current['Sub-sector']
-
-plt.barh(sub_sector, GHG_y, label='All Sectors', color=pal[5])
-
-plt.title("Impact of Canadian Green House Gases per Sector")
-plt.xlabel('Green House Gas Emission (%)')
-plt.tight_layout()
-plt.show()
-# -
-
-# ### Methane Emissions of Canada per Sector
-
-df_CH4 = pd.read_csv('Data/CH4_Emissions.csv')
-df_CH4_Can = df_CH4[df_CH4['Entity'].isin(['Canada'])].drop(
-    ['Code'], axis=1).query('Year >= 1900 & Year <= 2020')
-df_CH4_Can.rename(columns={
-    'Methane emissions from agriculture': 'Agriculture',
-    'Fugitive emissions of methane from energy production': 'Energy Production',
-    'Methane emissions from waste': 'Waste',
-    'Methane emissions from land use change and forestry': 'Forestry',
-    'Methane emissions from other fuel combustion': 'Fuel Combustion',
-    'Methane emissions from industry': 'Industry'}, inplace=True)
-
-
-
-# +
-CH4_y = df_CH4_Can.iloc[:, 2:]
-
-fig, ax = plt.subplots(figsize=(15, 8))
-
-ax.plot(df_CH4_Can['Year'], CH4_y)
-fig.autofmt_xdate(which='both')
-
-ax.set_title('Canadian Methane Gas Emission per Sector')
-ax.set_ylabel('Methane Gas Emission (Tonnes)')
-ax.set_xlabel('Year')
-ax.legend(['Agriculture', 'Forestry',
-           'Waste',
-           'Buildings',
-           'Industry',
-           'Manufacturing and Construction',
-           'Transport',
-           'Electricity and Heat',
-           'Energy Production',
-           'Fuel Combustion',
-           'Bunker Fuel'])
-plt.tight_layout()
-plt.show()
-# -
-
-# ### Current Methan Emissions (2020)
-
-# +
-
-df_Current2 = df_CH4_Can.query('Year == 2020')
-df_Current2 = df_Current2.drop(
-    ['Entity', 'Year'], axis=1).transpose().reset_index()
-df_Current2 = df_Current2.rename(
-    columns={'index': 'Sub-sector', 1022: 'CH4 Emission (Tonnes)'})
-df_Current2["CH4 Emission (%)"] = (df_Current2['CH4 Emission (Tonnes)'] /
-                                   sum(df_Current2['CH4 Emission (Tonnes)'])*100)
-df_Current2 = df_Current2.sort_values(by='CH4 Emission (%)')
-df_Current2
-
-# +
-CH4_y = df_Current2['CH4 Emission (%)']
-sub_sector = df_Current2['Sub-sector']
-
-plt.barh(sub_sector, CH4_y, label='All Sectors', color=pal[5])
-
-plt.title("Impact of Canadian Methan Emission per Sector")
-plt.xlabel('Methane Emission (Tonnes)')
-plt.tight_layout()
-plt.show()
-# -
-
-# ## CO2 Emission of Canada per Sector
-
-df_CO2 = pd.read_csv('Data/CO2_Emission.csv')
-df_CO2_Can = df_CO2[df_CO2['Entity'].isin(['Canada'])].drop(
-    ['Code'], axis=1).query('Year >= 1900 & Year <= 2020')
-df_CO2_Can.rename(columns={
-    'Carbon dioxide emissions from buildings': 'Buildings',
-    'Carbon dioxide emissions from industry': 'Industry',
-    'Carbon dioxide emissions from land use change and forestry': 'Forestry',
-    'Carbon dioxide emissions from other fuel combustion': 'Fuel Combustion',
-    'Carbon dioxide emissions from transport': 'Transport',
-    'Carbon dioxide emissions from manufacturing and construction': 'Manufacturing & Construction',
-    'Fugitive emissions of carbon dioxide from energy production': 'Energy Production',
-    'Carbon dioxide emissions from electricity and heat': 'Electricity & Heat'}, inplace=True)
-
-
-# +
-CO2_y = df_CO2_Can.iloc[:, 2:]
-
-fig, ax = plt.subplots(figsize=(15, 8))
-
-ax.plot(df_CO2_Can['Year'], CO2_y)
-fig.autofmt_xdate(which='both')
-
-ax.set_title('Canadian Carbon Dioxide Gas Emission per Sector')
-ax.set_ylabel('CO2 Gas Emission (Tonnes)')
-ax.set_xlabel('Year')
-ax.legend(['Buildings', 'Industry',
-           'Forestry',
-           'Fuel Combustion',
-           'Transport',
-           'Manufacturing & Construction',
-           'Energy Production',
-           'Electricity & Heat'])
-plt.tight_layout()
-plt.show()
-# -
-
-# ### Current CO2 Emissions (2020)
-
-# +
-
-df_Current3 = df_CO2_Can.query('Year == 2020')
-df_Current3 = df_Current3.drop(
-    ['Entity', 'Year'], axis=1).transpose().reset_index()
-df_Current3 = df_Current3.rename(
-    columns={'index': 'Sub-sector', 1022: 'CO2 Emission (Tonnes)'})
-df_Current3["CO2 Emission (%)"] = (df_Current3['CO2 Emission (Tonnes)'] /
-                                   sum(df_Current3['CO2 Emission (Tonnes)'])*100)
-df_Current3 = df_Current3.sort_values(by='CO2 Emission (%)')
-
-
-# +
-CO2_y = df_Current3['CO2 Emission (%)']
-sub_sector = df_Current3['Sub-sector']
-
-plt.barh(sub_sector, CO2_y, label='All Sectors', color=pal[5])
-
-plt.title("Impact of Canadian Carbon Dioxide Emission per Sector")
-plt.xlabel('CO2 Emission (%)')
-plt.tight_layout()
-plt.show()
-# -
-
-# # Nitrous Oxide (NOx) Emission of Canada per Sector
-
-df_NOx = pd.read_csv('Data/NOx_Emissions.csv')
-df_NOx_Can = df_NOx[df_NOx['Entity'].isin(['Canada'])].drop(
-    ['Code'], axis=1).query('Year >= 1900 & Year <= 2020')
-df_NOx_Can.rename(columns={
-    'Nitrous oxide emissions from agriculture': 'Agriculture',
-    'Nitrous oxide emissions from industry': 'Industry',
-    'Nitrous oxide emissions from other fuel combustion': 'Fuel Combustion',
-    'Nitrous oxide emissions from waste': 'Waste',
-    'Nitrous oxide emissions from land use change and forestry': 'Forestry',
-    'Fugitive emissions of nitrous oxide from energy production': 'Energy Production'}, inplace=True)
-
-
-# +
-NOx_y = df_NOx_Can.iloc[:, 2:]
-
-fig, ax = plt.subplots(figsize=(15, 8))
-
-ax.plot(df_NOx_Can['Year'], NOx_y)
-fig.autofmt_xdate(which='both')
-
-ax.set_title('Canadian Nitrous Oxides Emission per Sector')
-ax.set_ylabel('NOx Gas Emission (Tonnes)')
-ax.set_xlabel('Year')
-ax.legend(['Agriculture', 'Industry',
-           'Fuel Combustion',
-           'Waste',
-           'Forestry',
-           'Energy Production',
-           ])
-plt.tight_layout()
-plt.show()
-# -
-
-# ### Current NOx Emission (2020)
-
-df_Current4 = df_NOx_Can.query('Year == 2020')
-df_Current4 = df_Current4.drop(
-    ['Entity', 'Year'], axis=1).transpose().reset_index()
-df_Current4 = df_Current4.rename(
-    columns={'index': 'Sub-sector', 1022: 'NOx Emission (Tonnes)'})
-df_Current4["NOx Emission (%)"] = (df_Current4['NOx Emission (Tonnes)'] /
-                                   sum(df_Current4['NOx Emission (Tonnes)'])*100)
-df_Current4 = df_Current4.sort_values(by='NOx Emission (%)')
-
-
-# +
-NOx_y = df_Current4['NOx Emission (%)']
-sub_sector = df_Current4['Sub-sector']
-
-plt.barh(sub_sector, NOx_y, label='All Sectors', color=pal[5])
-
-plt.title("Impact of Canadian Nitrous Oxides Emission per Sector")
-plt.xlabel('NOx Emission (%)')
-plt.tight_layout()
-plt.show()
-# -
+df_GHG_Can['Year'] = pd.to_datetime(df_GHG_Can['Year'], format='%Y')
+with col2:
+    plost.line_chart(
+        data=df_GHG_Can,
+        x='Year',
+        y=('Agriculture', 'Forestry', 'Waste', 'Buildings',
+           'Industry', 'Manufacturing and Construction', 'Electricity and Heat', 'Energy Production', 'Fuel Combustion', 'Bunker Fuel'),
+        height=370,
+        legend='right',
+    )
 
 # # All GHG Together
+with col1:
+    df_All = pd.read_csv('Data/AllGHG_Can.csv')
+    palette = ['#b5838d', '#84a59d', '#f28482']
+    
+    def create_bar_chart(value,color=None):
+        plost.bar_chart(
+            data=df_All,
+            bar='Sub-sector',
+            value=value,
+            direction='horizontal',
+            legend='bottom',
+            height=400,
+            color=color,
+            use_container_width=True
+        )
 
-# +
+    if choose_GHG == 'CO2':
+        create_bar_chart('CO2 Emission (%)',palette[0])
+    elif choose_GHG == 'CH4':
+        create_bar_chart('CH4 Emission (%)', palette[1])
+    elif choose_GHG == 'NOx':
+        create_bar_chart('NOx Emission (%)', palette[2])
+    else:
+       create_bar_chart(
+           ['CO2 Emission (%)', 'CH4 Emission (%)', 'NOx Emission (%)'])
 
-frames = [df_Current2, df_Current3, df_Current4]
-df_All = pd.merge(pd.merge(df_Current2, df_Current3, how='outer',
-                  on='Sub-sector'), df_Current4, how='outer', on='Sub-sector').fillna(0)
-df_All = df_All.sort_values(by='Sub-sector')
-df_All
-
-# +
-sub_sector = df_All['Sub-sector']
-
-plt.barh(sub_sector, df_All['CO2 Emission (%)'],
-         label='CO2 Emissions', color=pal[3])
-plt.barh(sub_sector, df_All['NOx Emission (%)'],
-         label='NOx Emissions', color=pal[1])
-plt.barh(sub_sector, df_All['CH4 Emission (%)'],
-         label='CH4 Emissions', color=pal[5])
-
-plt.title("Impact of Canadian Emission per Sector")
-plt.xlabel('GHG Emission (%)')
-plt.legend()
-plt.tight_layout()
-plt.show()
-# -
-
-# ### Comparison to the World
